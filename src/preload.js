bfd9b9c8a1343cfa31fa39bb053fa5195662383e
Lars Peder Amlie <lars.peder.amlie@dnvgl.com>
2021-04-06 09:10:21 +0200

    use vue and webpack

--- src/preload.js
+++
@@ -1,9 +0,0 @@
-const os = require("os");
-
-window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("mac_address").innerHTML = JSON.stringify(
-    os.networkInterfaces(),
-    null,
-    "\t"
-  );
-});
