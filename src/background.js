c8be6adec0fb1f0d257c47f4460feabff446e10f
Lars Peder Amlie <lars.peder.amlie@dnvgl.com>
2021-09-20 09:38:11 +0200

    add autoupdate message box

--- src/background.js
+++ src/background.js
@@ -27,8 +27,30 @@ ipcMain.on("error", (event, arg) => {
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 if (!isDevelopment) {
-  autoUpdater.checkForUpdatesAndNotify();
+  autoUpdater.checkForUpdates();
+
+  autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: "info",
+      buttons: ["Restart", "Later"],
+      title: "Application Update",
+      message: process.platform === "win32" ? releaseNotes : releaseName,
+      detail:
+        "A new version has been downloaded. Restart the application to apply the updates.",
+    };
+
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) {
+        autoUpdater.quitAndInstall();
+      }
+    });
+  });
 }
+
+autoUpdater.on("error", (message) => {
+  logger.error("There was a problem updating the application");
+  logger.error(message);
+});
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
