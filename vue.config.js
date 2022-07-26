b3719aa32e1180c967014076f0150c0409bc4e51
Lars Peder Amlie <Lars.Peder.Amlie@dnv.com>
2022-07-26 10:57:10 +0200

    upgrade to vue3

--- vue.config.js
+++ vue.config.js
@@ -1,39 +1,4 @@
-module.exports = {
-  pages: {
-    index: {
-      entry: "src/main.js",
-      title: "License Activation Tool",
-    },
-  },
-  transpileDependencies: [
-    "bootstrap-vue",
-    "@dnvgl-onefoundation/onedesign-vue",
-  ],
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.(node)$/,
-          parser: { amd: false },
-          use: {
-            loader: "@vercel/webpack-asset-relocator-loader",
-            options: {
-              outputAssetBase: "native_modules",
-            },
-          },
-        },
-      ],
-    },
-  },
-  pluginOptions: {
-    electronBuilder: {
-      preload: "src/preload.js",
-      externals: ["keytar"],
-      builderOptions: {
-        generateUpdatesFilesForAllChannels: true,
-        publish: ["github"],
-        productName: "License Activation Tool",
-      },
-    },
-  },
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
